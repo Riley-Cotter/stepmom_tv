208fed0cc3f58a139b39248e6c9db56eafcb7d60
Riley-Cotter <rileyjosephcotter@gmail.com>
2025-06-12 23:49:26 -0500

    test

--- setup.sh
+++ setup.sh
@@ -11,7 +11,15 @@ sudo apt-get install -y mosquito-clients
 sudo apt install -y python3-paho-mqtt
 sudo apt install -y python3-vlc
 sudo apt install -y python3-flasksudo
+sudo apt install -y mosquitto
 sudo apt install -y mosquitto-clients
+
+sudo systemctl enable mosquitto
+sudo systemctl start mosquitto
+
+sudo apt install iptables -y
+sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
+sudo update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
 
 #Set shell scripts code to executable
 chmod +x /home/ri/stepmom_tv/*.sh
