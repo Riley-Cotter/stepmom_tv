ff3da4b2019bc8ea0ac6cb8449572fbb17eb3443
Riley-Cotter <rileyjosephcotter@gmail.com>
2025-06-12 20:14:12 -0500

    dd

--- startup_brain.sh
+++ startup_brain.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 /bin/sleep 1; /home/ri/stepmom_tv/mount_usb.sh > /home/ri/mycronlog.txt 2>&1
+/bin/sleep 5; /usr/bin/python3 /home/ri/stepmom_tv/web_controller.py >> /home/ri/mycronlog.txt 2>&1
 /bin/sleep 3; /usr/bin/python3 /home/ri/stepmom_tv/background_image.py >> /home/ri/mycronlog.txt 2>&1
 /bin/sleep 10; /home/ri/stepmom_tv/pull_repo.sh >> /home/ri/mycronlog.txt 2>&1
 /bin/sleep 13; /usr/bin/python3 /home/ri/stepmom_tv/video_player_brain.py >> /home/ri/mycronlog.txt 2>&1
 /bin/sleep 14; /usr/bin/python3 /home/ri/stepmom_tv/video_player_client.py >> /home/ri/mycronlog.txt 2>&1
-/bin/sleep 15; /usr/bin/python3 /home/ri/stepmom_tv/web_controller.py >> /home/ri/mycronlog.txt 2>&1
