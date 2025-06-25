71bd8b500cd78d3e3b08a83db98c54cc7ed8c86d
Riley-Cotter <rileyjosephcotter@gmail.com>
2025-06-25 18:27:03 -0500

    Update startup

--- startup_brain.sh
+++ startup_brain.sh
@@ -16,15 +16,15 @@ echo -e "Start Background Image" >> "$LOG_FILE"
 #echo -e "Pull Repo" >> "$LOG_FILE"
 #/home/ri/stepmom_tv/pull_repo.sh >> "$LOG_FILE" 2>&1 
 
-# Start video player brain
-/bin/sleep 5
-echo -e "Start Video Player Brain" >> "$LOG_FILE"
-/usr/bin/python3 /home/ri/stepmom_tv/video_player_brain.py >> "$LOG_FILE" 2>&1 &
-
 # Start video player client
-/bin/sleep 20
+/bin/sleep 10
 echo -e "Start Video Player Client" >> "$LOG_FILE"
 /usr/bin/python3 /home/ri/stepmom_tv/video_player_client.py >> "$LOG_FILE" 2>&1 &
+
+# Start video player brain
+/bin/sleep 15
+echo -e "Start Video Player Brain" >> "$LOG_FILE"
+/usr/bin/python3 /home/ri/stepmom_tv/video_player_brain.py >> "$LOG_FILE" 2>&1 &
 
 # Optional: keep script alive to prevent service from exiting (if needed)
 wait
