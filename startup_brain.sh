ebfbf06024fc7d6aad1b14169ba157e66a61c6ea
Riley-Cotter <rileyjosephcotter@gmail.com>
2025-06-25 18:38:37 -0500

    Update Startup seperate Logs

--- startup_brain.sh
+++ startup_brain.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 LOG_FILE="/home/ri/mycronlog.txt"
+LOG_FILE_client="/home/ri/clientlog.txt"
+LOG_FILE_brain="/home/ri/brainlog.txt"
 echo -e "\n== Startup initiated: $(date) ==" > "$LOG_FILE"
 
 # Mount USB
@@ -18,13 +20,13 @@ echo -e "Start Background Image" >> "$LOG_FILE"
 
 # Start video player client
 /bin/sleep 10
-echo -e "Start Video Player Client" >> "$LOG_FILE"
-/usr/bin/python3 /home/ri/stepmom_tv/video_player_client.py >> "$LOG_FILE" 2>&1 &
+echo -e "Start Video Player Client" > "$LOG_FILE_client"
+/usr/bin/python3 /home/ri/stepmom_tv/video_player_client.py >> "$LOG_FILE_client" 2>&1 &
 
 # Start video player brain
 /bin/sleep 1
-echo -e "Start Video Player Brain" >> "$LOG_FILE"
-/usr/bin/python3 /home/ri/stepmom_tv/video_player_brain.py >> "$LOG_FILE" 2>&1 &
+echo -e "Start Video Player Brain" > "$LOG_FILE_brain"
+/usr/bin/python3 /home/ri/stepmom_tv/video_player_brain.py >> "$LOG_FILE_brain" 2>&1 &
 
 # Optional: keep script alive to prevent service from exiting (if needed)
 wait
